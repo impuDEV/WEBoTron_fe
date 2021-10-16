5263fd30b075eb95d03774ad8e7fdedf5b9de067
impuDEV <impudev@gmail.com>
2021-10-17 00:47:39 +0300

    Init structure simplifyed (w/o styles APIs)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {BrowserRouter as Router} from 'react-router-dom'
 import {useRoutes} from './routes'
 import {Navbar} from "./components/Navbar";
-import 'materialize-css'
 
 function App() {
     const routes = useRoutes()
